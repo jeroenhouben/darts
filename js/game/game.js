43a0dd7de07c862fedbba84d6d7e63bbef3561fe
Jeroen Houben <jeroen@supercool.nl>
2013-01-28 17:58:31 +0100

    game.js yeah

---
+++ js/game/game.js
@@ -0,0 +1,34 @@
+/*
+* Game 
+*/
+function Game() {
+
+	this.players = [];
+	
+}
+Game.prototype = {
+	
+	addPlayer: function(player) {
+		
+	}
+		
+}
+
+
+/*
+* Player 
+*/
+function Player() {
+	
+}
+Player.prototype = {
+			
+}
+
+
+/*
+* init new game
+*/
+{};
+exports.Game = Game;
+exports.Player = Player;
