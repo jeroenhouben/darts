1c68932d0093e4c0fb36b2dc4fb9453315bc50fd
Jeroen <jeroenhouben@dweazbookpreu.home>
2013-01-28 22:49:34 +0100

    strak

--- js/app.js
+++ js/app.js
@@ -1,11 +1,18 @@
 $(document).ready(function() {
 
-  var numpad = $('#numpad'),
-      home   = $('#home')
+  var numpad     = $('#numpad'),
+      home       = $('#home'),
+      scoreboard = $('#scoreboard')
   
   numpad.on('click', 'button', function() {
     $('#multipliers-modal').modal({})
   });
+
+  scoreboard.on('click', 'td', function() {
+    $('#score-entry-choice-modal').modal({})
+  });
+
+
   
   home.find('.btn-group .btn301').button('toggle');
   
