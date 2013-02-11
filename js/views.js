7154334c68318087e2795bf1471d3d9d2bdc22f1
Jeroen Houben <jeroen@supercool.nl>
2013-02-11 17:21:11 +0100

    start touchin

--- js/views.js
+++ js/views.js
@@ -1,10 +1,59 @@
 // views
+
+
+/*
+*
+*/
+App.MatchSetupView = Ember.View.extend({
+  templateName: 'match/setup',
+
+  touchStart: function(e) {
+    this.handleClick(e);
+  },
+  
+  click: function(e) {
+    this.handleClick(e);
+  },
+  
+  handleClick: function(e) {
+    var btn = $(e.target).closest('button');
+    if (!btn.length) {
+      return true;
+    }
+    e.preventDefault();
+
+    // startscore selected
+    if (btn.closest('.start-score-select').length) {
+      this.controller.set('startScore',  parseInt(btn.text(), 10));
+
+    // players selected
+    } else if (btn.closest('.player-select').length) {
+      this.controller.initNumberOfPlayers(parseInt(btn.data('players'), 10));
+    }
+
+  }
+  
+
+
+});
+
+/*
+*
+*/
 App.TurnOnScoreboardView = Ember.View.extend({
   tagName: 'li',
   templateName: 'turn-on-scoreboard',
   isEditing: false,
+
+  touchStart: function(e) {
+    this.handleClick(e);
+  },
   
-  click: function(event) {
+  click: function(e) {
+    this.handleClick(e);
+  },
+  
+  handleClick: function(e) {
     this.set('isEditing', (!this.isEditing));
     if (this.get('isEditing')) {
       this.get('controller').startCalculator(this.get('context'));
@@ -13,6 +62,9 @@ App.TurnOnScoreboardView = Ember.View.extend({
 
 });
 
+/*
+*
+*/
 App.TurnCalculatorView = Ember.View.extend({
   templateName: 'calc'
 });
