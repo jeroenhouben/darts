d75f163f79378566e3dcb2dd3930c3ae17a5c7e1
Jeroen Houben <jeroen@supercool.nl>
2013-02-16 12:48:43 +0100

    fix sampledata

--- js/sampledata.js
+++ js/sampledata.js
@@ -1,39 +1,41 @@
-App.Leg.FIXTURES = [{
-  id: 1,
-  startScore: 501
-}];
-
 // App.Leg.FIXTURES = [{
 //   id: 1,
-//   startScore: 501,
-//   players: [100,200]
+//   startScore: 501
 // }];
-// 
-// App.Player.FIXTURES = [{
-//   id: 100,
-//   name: "Jeroen",
-//   turns: [1000,1001]
-// },{
-//   id: 200,
-//   name: "Starvin",
-//   turns: []
-// }
-// ];
-// 
-// App.Turn.FIXTURES = [{
-//   player: 100,
-//   id: 1000,
-//   dart1: 1,
-//   dart2: 1,
-//   dart3: 1,
-//   completed: true
-// },{
-//   player: 100,
-//   id: 1001,
-//   dart1: 2,
-//   dart2: 2,
-//   dart3: 2,
-//   completed: true
-// }
-// 
-// ];
+
+App.Leg.FIXTURES = [{
+  id: 1,
+  startScore: 501,
+  players: [100,200]
+}];
+
+App.Player.FIXTURES = [{
+  leg: 1,
+  id: 100,
+  name: "Jeroen",
+  turns: [1000]
+},{
+  leg: 1,
+  id: 200,
+  name: "Starvin",
+  turns: [1001]
+}
+];
+
+App.Turn.FIXTURES = [{
+  player: 100,
+  id: 1000,
+  dart1: 1,
+  dart2: 1,
+  dart3: 1,
+  completed: true
+},{
+  player: 200,
+  id: 1001,
+  dart1: 2,
+  dart2: 2,
+  dart3: 2,
+  completed: true
+}
+
+];
